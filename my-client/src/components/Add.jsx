021e7f738b42bd1e5423446fc1409e51dff875d7
Niharika210 <rika212006@gmail.com>
2025-07-29 16:11:19 +0530

    Update Add.jsx

--- my-client/src/components/Add.jsx
+++ my-client/src/components/Add.jsx
@@ -13,7 +13,7 @@ export  function Add(){
         e.preventDefault();
         try
         {
-           const response = await axios.post('http://localhost:3002/api/employees',
+           const response = await axios.post('https://my-project-m8st.onrender.com/api/employees',
             {empNo,empName,empSal});
             //alert(JSON.stringify(response.data,null,2));
             alert(response.data.message);
